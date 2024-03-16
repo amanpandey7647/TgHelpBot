9d8048acb916d2c2641f6722a1a6e09614152fc9
Aman Kumar Pandey <160414387+amanpandey7647@users.noreply.github.com>
2024-03-16 11:53:57 +0530

    Client

--- Botique/system/clients/client.py
+++ Botique/system/clients/client.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from telethon import TelegramClient
 import telethon.utils
 import telethon.events
-
+from Botique.services.funcs import *
 
 class ReverseList(list):
     def __iter__(self):
@@ -36,4 +36,113 @@ class Botique(TelegramClient):
         self._event_builders = ReverseList()
 
         self.loop.run_until_complete(self._async_init(bot_token=bot_token))
+                
+    async def _async_init(self, **kwargs):
+        await self.start(**kwargs)
 
+        self.me = await self.get_me()
+        self.uid = telethon.utils.get_peer_id(self.me)
+
+        core_plugin = Path(__file__).parent / "_core.py"
+        await self.load_plugin_from_file(core_plugin)
+
+        for p in Path().glob(f"{self._plugin_path}/*.py"):
+            await self.load_plugin_from_file(p)
+
+    async def load_plugin(self, shortname):
+        await self.load_plugin_from_file(f"{self._plugin_path}/{shortname}.py")
+
+    async def load_plugin_from_file(self, path):
+        path = Path(path)
+        shortname = path.stem
+        name = f"_BotiquePlugins.{self._name}.{shortname}"
+
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+
+        mod.BOTIQUE = self
+        mod.logger = logging.getLogger(shortname)
+        mod.storage = self.storage(f"{self._name}/{shortname}")
+
+        try:
+            spec.loader.exec_module(mod)
+        except util.StopImport:
+            return
+        self._plugins[shortname] = mod
+
+        if callable(getattr(mod, 'load', None)):
+            try:
+                unload = mod.unload()
+                if inspect.isawaitable(unload):
+                    await unload
+            except Exception:
+                self._logger.exception(f'Unhandled exception loading {shortname}')
+
+        self._logger.info(f"Successfully loaded plugin {shortname}")
+
+    async def remove_plugin(self, shortname):
+        name = self._plugins[shortname].__name__
+
+        for i in reversed(range(len(self._event_builders))):
+            ev, cb = self._event_builders[i]
+            if cb.__module__ == name:
+                del self._event_builders[i]
+
+        plugin = self._plugins.pop(shortname)
+        if callable(getattr(plugin, 'unload', None)):
+            try:
+                unload = plugin.unload()
+                if inspect.isawaitable(unload):
+                    await unload
+            except Exception:
+                self._logger.exception(f'Unhandled exception unloading {shortname}')
+
+        del plugin
+        self._logger.info(f"Removed plugin {shortname}")
+
+    def await_event(self, event_matcher, filter=None):
+        fut = asyncio.Future()
+
+        @self.on(event_matcher)
+        async def cb(event):
+            try:
+                if filter is None or await filter(event):
+                    fut.set_result(event)
+            except telethon.events.StopPropagation:
+                fut.set_result(event)
+                raise
+
+        fut.add_done_callback(
+            lambda _: self.remove_event_handler(cb, event_matcher))
+
+        return fut
+
+    def cmd(self, command, pattern=None, flags="", admin_only=False):
+        command = fr'(?:{command})'
+        if self.me.bot:
+            command = fr'{command}(?:@{self.me.username})?'
+
+        if pattern is not None:
+            pattern = fr'{command}{pattern}'
+        else:
+            pattern = command
+
+        if not self.me.bot:
+            pattern = fr'^\.{pattern}'
+        else:
+            pattern = fr'^\/{pattern}'
+        pattern = fr'(?i{flags}){pattern}$'
+
+        if self.me.bot and admin_only:
+            allowed_users = self.admins
+        else:
+            allowed_users = None
+
+        return telethon.events.NewMessage(
+            outgoing=not self.me.bot,
+            from_users=allowed_users,
+            pattern=pattern
+        )
+
+    def botique_cmd(self, command, pattern=None, flags=""):
+        return self.cmd(command, pattern, admin_only=True)
