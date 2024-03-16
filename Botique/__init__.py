5c7c202ff16ac80acdddd7b59e11dcb83dc0a941
Aman Kumar Pandey <160414387+amanpandey7647@users.noreply.github.com>
2024-03-16 12:07:56 +0530

    Update __init__.py

--- Botique/__init__.py
+++ Botique/__init__.py
@@ -1,3 +1,18 @@
 import os
 import sys
 from .services import *
+import logging
+
+import api_key
+
+logging.basicConfig(level=logging.INFO)
+
+BOTIQUE = Botique(
+    "stdborg",
+    plugin_path="stdplugins",
+    connection_retries=None,
+    api_id=api_key.id,
+    api_hash=api_key.hash
+)
+
+BOTIQUE.run_until_disconnected()
